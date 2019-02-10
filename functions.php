420aa6b30dfdc893690366077f5bef7974fe86b9
cigdemertugrul <ciqddeeemm@hotmail.com>
2019-02-10 19:18:05 +0300

    Foundation Reveal Widget

--- functions.php
+++ functions.php
@@ -26,3 +26,70 @@ endif;
 add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 20 );
 
 // END ENQUEUE PARENT ACTION 
+//-----------------------FOUNDATION REVEAL WIDGET---------------------------------//
+
+class foundationReveal extends WP_Widget {
+	public function __construct()
+{
+    parent::__construct('widget_freveal', 'Foundation Reveal', [
+        'classname' => 'Foundation Reveal',
+        'description' => 'Foundation Reveal ile sayfa göstermenizi sağlar.',
+        'customize_selective_refresh' => true,
+    ]);
+}
+
+public function form($instance)
+{
+    //Admin kısmındaki form alanını oluşturur.    
+    $fr_url=!empty($instance['fr_url'])? $instance['fr_url'] : '#';
+    $fr_title=!empty($instance['fr_title'])? $instance['fr_title'] : '';
+   ?>
+   <label for="<?php echo $this->get_field_id('fr_title')?>">Başlık :</label>
+   <input class="widefat" type="" name="<?php echo $this->get_field_name('fr_title')?>" id="<?php echo $this->get_field_id('fr_title')?>" value="<?php echo $fr_title;?>">
+   
+   <label for="<?php echo $this->get_field_id('fr_url')?>">Sayfa Url :</label>
+   <input class="widefat" type="" name="<?php echo $this->get_field_name('fr_url')?>" id="<?php echo $this->get_field_id('fr_url')?>" value="<?php echo $fr_url;?>">
+   <?php
+    
+}
+//update fonksiyonunda forma girilen değerleri eski değerlerle güncelliyorum.
+public function update($new_instance, $old_instance)
+{
+ $old_instance['fr_title']=$new_instance['fr_title'];
+ $old_instance['fr_url']=$new_instance['fr_url'];
+ return $old_instance;
+}
+//Temada görünen kısmı bu fonksiyonda yazılır.
+public function widget($args, $instance)
+{ 
+    
+    $title=apply_filters('widget_title',$instance['fr_title']);    
+    $w_url=$instance['fr_url'];
+    
+    echo $args['before_title'].$args['before_widget'];
+    
+    ?>
+<h6><a data-open="exampleModal1" aria-controls="exampleModal1" aria-haspopup="true" tabindex="0"><?php echo $title;?></a></h6>
+
+<div class="reveal" id="exampleModal1" data-reveal>
+     <div id="revealdiv" class="main-content">
+     
+    </div>
+	<button class="close-button" data-close aria-label="Close reveal" type="button">
+	<span aria-hidden="true">&times;</span>
+	</button>                    
+</div>
+<script>
+$("#revealdiv").empty();
+$( "#revealdiv" ).load( "<?php echo $w_url;?> .main-content");
+</script>
+
+<?php
+echo  $args['after_title'].$args['after_widget'];  
+          }
+}
+//Widgeti adminde göstermek ,tanıtmak için 
+function foundation_reveal_widget(){
+	register_widget('foundationReveal');
+}
+add_action('widgets_init','foundation_reveal_widget');
